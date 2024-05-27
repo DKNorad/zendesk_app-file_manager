a767f687caec5c96b4d6448d61ad91c1f797e63c
DKNorad <nedd.petrov@gmail.com>
2024-05-27 20:16:30 +0300

    Fixed the file view modal

--- src/typescripts/components/Modal.tsx
+++ src/typescripts/components/Modal.tsx
@@ -1,34 +1,7 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 
 const Modal: React.FC = () => {
-    const [blobObjectUrl, setBlobObjectUrl] = useState<string>("")
-    const contentUrl = parseParams(window.location.hash)
-
-    useEffect(() => {
-        const readFile = async () => {
-            try {
-                console.log(contentUrl)
-                setBlobObjectUrl(contentUrl)
-            } catch (error) {
-                console.error("Failed to open the file:", error)
-            }
-        }
-
-        readFile()
-    }, [contentUrl])
-
-    function parseParams(paramString: string): string {
-        return paramString.slice(6)
-    }
-
-    return (
-        <div style={{ height: "99vh", width: "98vw" }}>
-            <iframe
-                style={{ height: "100%", width: "100%", border: "none" }}
-                src={blobObjectUrl}
-            ></iframe>
-        </div>
-    )
+    return <div />
 }
 
 export default Modal
--- src/typescripts/components/ModalContent.tsx
+++ src/typescripts/components/ModalContent.tsx
@@ -1,34 +1,16 @@
-// src/components/ModalContent.tsx
 import React from "react"
 
 interface ModalContentProps {
-    data: {
-        contentType: string
-        contentUrl: string
-        fileName: string
-        size: number
-        timestamp: string
-    }
+    data: string
 }
 
 const ModalContent: React.FC<ModalContentProps> = ({ data }) => {
-    return (
-        <div>
-            <h2>{data.fileName}</h2>
-            <p>Content Type: {data.contentType}</p>
-            <p>Size: {data.size} bytes</p>
-            <p>Timestamp: {data.timestamp}</p>
-            {data.contentUrl && (
-                <a
-                    href={data.contentUrl}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    View File
-                </a>
-            )}
-        </div>
-    )
+    const styles = {
+        fontSize: "16px",
+        fontWeight: 400,
+    }
+
+    return <pre style={styles}>{data}</pre>
 }
 
 export default ModalContent
