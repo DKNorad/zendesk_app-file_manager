0c8e041be083a68dfceeedf4e1ed8f1e2f49083b
DKNorad <nedd.petrov@gmail.com>
2024-05-31 23:10:34 +0300

    Completed the file delete modals logic and error handling

--- src/typescripts/components/OverflowMenu.tsx
+++ src/typescripts/components/OverflowMenu.tsx
@@ -2,9 +2,10 @@ import React, { useCallback, useState } from "react"
 import { Menu, Item } from "@zendeskgarden/react-dropdowns.next"
 import { Button } from "@zendeskgarden/react-buttons"
 import { collectedAttachmens } from "./NavTabs"
-import ConfirmDeleteModal from "./DeleteModal"
+import ConfirmDeleteModal from "./ConfirmDeleteModal"
+import DeleteInformationModal from "./DeleteInformationModal"
 import { getZendeskClient } from "./ZenDeskClient"
-import ModalContent from "./ModalContent"
+import Modal from "./Modal"
 import { renderToString } from "react-dom/server"
 
 interface Props {
@@ -12,45 +13,74 @@ interface Props {
     fileType: string
 }
 
+export interface DeletionError {
+    error: string
+    description: string
+}
+
 const OverflowMenu: React.FC<Props> = ({ attachment, fileType }) => {
     // For the delete confirmation modal
-    const [isModalVisible, setModalVisible] = useState(false)
+    const [isConfirmDeleteModalVisible, setConfirmDeleteModalVisible] =
+        useState(false)
+    const [
+        isDeleteInformationModalVisible,
+        setisDeleteInformationModalVisible,
+    ] = useState(false)
     const [selectedFileName, setSelectedFileName] = useState("")
+    const [deletionError, setDeletionError] = useState<
+        DeletionError | undefined
+    >()
 
     // For the file view modal
-    const openFileModal = useCallback(async (url: string) => {
-        try {
-            const client = getZendeskClient()
-            client
-                .invoke("instances.create", {
+    const openFileModal = useCallback(
+        async (url: string) => {
+            try {
+                const client = getZendeskClient()
+                const context = await client.context()
+                const parent_guid = context.instanceGuid
+
+                const modalContext = await client.invoke("instances.create", {
                     location: "modal",
-                    url: "assets/modal.html",
-                    size: {
-                        width: "80vw",
-                        height: "70vh",
-                    },
+                    url: `assets/modal.html#parent_guid=${parent_guid}`,
+                    size: { width: "80vw", height: "70vh" },
                 })
-                .then((modalContext: any) => {
-                    const modalClient = client.instance(
-                        modalContext["instances.create"][0].instanceGuid,
+
+                const modalClient = client.instance(
+                    modalContext["instances.create"][0].instanceGuid,
+                )
+
+                const setHtml = () => {
+                    const modalContentString = renderToString(
+                        fileType === "image" ? (
+                            <Modal
+                                data={{
+                                    url: url,
+                                    fileName: attachment.fileName,
+                                }}
+                                fileType="image"
+                            />
+                        ) : (
+                            <Modal
+                                data={{
+                                    url: url,
+                                    fileName: attachment.fileName,
+                                }}
+                                fileType="text"
+                            />
+                        ),
                     )
-                    client.on("modalReady", function setHtml() {
-                        const modalContentString = renderToString(
-                            fileType === "image" ? (
-                                <ModalContent data={url} fileType="image" />
-                            ) : (
-                                <ModalContent data={url} fileType="text" />
-                            ),
-                        )
-                        modalClient.trigger("drawData", modalContentString)
-                        client.off("modalReady", setHtml)
-                    })
-                    modalClient.on("modal.close", function () {})
-                })
-        } catch (error) {
-            console.error("Failed to open modal:", error)
-        }
-    }, [])
+                    modalClient.trigger("drawData", modalContentString)
+                    client.off("modalReady", setHtml)
+                }
+
+                client.on("modalReady", setHtml)
+                modalClient.on("modal.close", () => {})
+            } catch (error) {
+                console.error("Failed to open modal:", error)
+            }
+        },
+        [fileType],
+    )
 
     const openFile = useCallback(async (openType: string) => {
         try {
@@ -81,24 +111,60 @@ const OverflowMenu: React.FC<Props> = ({ attachment, fileType }) => {
 
     const deleteAttachment = useCallback(async () => {
         try {
-            const client = (window as any).ZAFClient.init()
+            const client = getZendeskClient()
             const options = {
                 url: `/api/v2/tickets/${attachment.ticketID}/comments/${attachment.messageID}/attachments/${attachment.attachmentID}/redact`,
                 type: "PUT",
                 contentType: "application/json",
             }
 
-            await client.request(options)
-            setModalVisible(false)
-        } catch (error) {
-            console.error("Failed to redact attachment:", error)
+            // Define a timeout duration in milliseconds
+            const TIMEOUT_DURATION = 5000 // 5 seconds
+
+            // Create a promise that resolves after the timeout duration
+            const timeoutPromise = new Promise<void>((_, reject) => {
+                setTimeout(() => {
+                    reject(new Error("Request timed out"))
+                }, TIMEOUT_DURATION)
+            })
+
+            // Create a promise for the API request
+            const requestPromise = client.request(options)
+
+            // Use Promise.race to race between the request and the timeout
+            await Promise.race([requestPromise, timeoutPromise])
+
+            // If the request succeeds, update state accordingly
+            setConfirmDeleteModalVisible(false)
+            setisDeleteInformationModalVisible(true)
+        } catch (error: any) {
+            if (
+                error &&
+                error.responseJSON &&
+                typeof error.responseJSON === "object"
+            ) {
+                const responseJSON = error.responseJSON as DeletionError
+                setDeletionError(responseJSON)
+                setConfirmDeleteModalVisible(false)
+                setisDeleteInformationModalVisible(true)
+            } else {
+                console.error("Failed to delete attachment:", error)
+            }
         }
     }, [attachment.attachmentID, attachment.messageID, attachment.ticketID])
 
     const handleDeleteClick = useCallback(() => {
         setSelectedFileName(attachment.fileName)
-        setModalVisible(true)
+        setConfirmDeleteModalVisible(true)
     }, [attachment.fileName])
+
+    const handleViewNewTabClick = () => {
+        if (fileType === "image") {
+            window.open(attachment.contentUrl, "_blank")
+        } else {
+            openFile("NewTab")
+        }
+    }
 
     return (
         <>
@@ -112,7 +178,7 @@ const OverflowMenu: React.FC<Props> = ({ attachment, fileType }) => {
                 <Item value="view" onClick={() => openFile("Modal")}>
                     View
                 </Item>
-                <Item value="viewNewTab" onClick={() => openFile("NewTab")}>
+                <Item value="viewNewTab" onClick={handleViewNewTabClick}>
                     View (New Tab)
                 </Item>
                 {fileType !== "image" && (
@@ -130,10 +196,16 @@ const OverflowMenu: React.FC<Props> = ({ attachment, fileType }) => {
                 </Item>
             </Menu>
             <ConfirmDeleteModal
-                visible={isModalVisible}
-                onClose={() => setModalVisible(false)}
+                visible={isConfirmDeleteModalVisible}
+                onClose={() => setConfirmDeleteModalVisible(false)}
                 onDelete={deleteAttachment}
                 fileName={selectedFileName}
+            />
+            <DeleteInformationModal
+                visible={isDeleteInformationModalVisible}
+                onClose={() => setisDeleteInformationModalVisible(false)}
+                fileName={selectedFileName}
+                deletionError={deletionError}
             />
         </>
     )
