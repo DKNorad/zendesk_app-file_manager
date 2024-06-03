076195757e440e8ae54c703130e68657c45fdf10
DKNorad <nedd.petrov@gmail.com>
2024-06-03 19:35:55 +0300

    Fixed typescript type checking errors.

--- src/typescripts/components/NavTabs.tsx
+++ src/typescripts/components/NavTabs.tsx
@@ -1,144 +1,28 @@
 import React, { useEffect, useState } from "react"
 import { Tabs, TabList, Tab, TabPanel } from "@zendeskgarden/react-tabs"
 import FilesTable from "./FilesTable"
-import { getZendeskClient } from "./ZenDeskClient"
+import { getZendeskClient, getSubDomain } from "./ZenDeskClient"
 import ImagesTable from "./ImagesTable"
 import LoaderSkeleton from "./loaders/LoaderSkeleton"
+import {
+    commentObject,
+    commentsObject,
+    collectedAttachmens,
+    CollectedEmbeddedImages,
+} from "../utils/interfaces"
 
 const zafClient = getZendeskClient()
 
-// Defining the interfaces for the objects returned by the API.
-interface commentsObject {
-    comments: commentObject[]
-    count: number
-    next_page: string | null
-    previous_page: string | null
-}
-interface commentObject {
-    attachments: Array<attachment>
-    audit_id: number
-    author_id: number
-    body: string
-    created_at: string
-    html_body: string
-    id: number
-    metadata: object
-    plain_body: string
-    public: boolean
-    type: string
-    via: object
-}
-
-interface thumbnail {
-    content_type: string
-    content_url: string
-    deleted: boolean
-    file_name: string
-    height: number | null
-    width: number | null
-    id: number
-    inline: boolean
-    malware_access_verride: boolean
-    malware_scan_results: string
-    mapped_content_url: string
-    size: number
-    url: string
-}
-
-interface attachment {
-    content_type: string
-    content_url: string
-    deleted: boolean
-    file_name: string
-    height: number | null
-    width: number | null
-    id: number
-    inline: boolean
-    malware_access_verride: boolean
-    malware_scan_results: string
-    mapped_content_url: string
-    size: number
-    thumbnails: Array<null | thumbnail>
-    url: string
-}
-
-export interface collectedAttachmens {
-    contentType: string
-    contentUrl: string
-    fileName: string
-    height: number | null
-    width: number | null
-    size: number
-    timestamp: string
-    messageID: number
-    ticketID: number
-    attachmentID: number
-    thumbnails: Array<null | thumbnail>
-}
-
-async function getMimeTypeFromUrl(url: string): Promise<string> {
-    try {
-        const response = await fetch(url)
-        if (!response.ok) {
-            throw new Error(
-                `Network response was not ok: ${response.statusText}`,
-            )
-        }
-
-        const buffer = await response.arrayBuffer()
-        const arr = new Uint8Array(buffer).subarray(0, 4)
-        let header = ""
-        for (let i = 0; i < arr.length; i++) {
-            header += arr[i].toString(16)
-        }
-
-        let riffType: Uint8Array | undefined
-        let riffHeader = ""
-
-        switch (header) {
-            case "89504e47": // PNG
-                return "image/png"
-            case "47494638": // GIF
-                return "image/gif"
-            case "ffd8ffe0": // JPEG
-            case "ffd8ffe1":
-            case "ffd8ffe2":
-                return "image/jpeg"
-            case "424d": // BMP
-                return "image/bmp"
-            case "49492a00": // TIFF (Little Endian)
-            case "4d4d002a": // TIFF (Big Endian)
-                return "image/tiff"
-            case "52494646": // WebP and others that start with RIFF
-                riffType = new Uint8Array(buffer).subarray(8, 12)
-                riffHeader = ""
-                for (let i = 0; i < riffType.length; i++) {
-                    riffHeader += String.fromCharCode(riffType[i])
-                }
-                if (riffHeader === "WEBP") {
-                    return "image/webp"
-                }
-                break
-            case "3c3f786d": // SVG (SVG images are text files that start with '<?xml')
-                return "image/svg+xml"
-            default:
-                return "unknown"
-        }
-
-        // Additional check for SVG as it might start with other characters
-        const text = new TextDecoder().decode(buffer)
-        if (text.startsWith("<?xml") || text.startsWith("<svg")) {
-            return "image/svg+xml"
-        }
-
-        return "unknown"
-    } catch (error) {
-        console.error("Failed to fetch or read the file:", error)
-        return "unknown"
-    }
-}
-
-async function getCommentData() {
+async function getCommentData(): Promise<
+    | [
+          collectedAttachmens[],
+          CollectedEmbeddedImages[],
+          collectedAttachmens[],
+          collectedAttachmens[],
+          collectedAttachmens[],
+      ]
+    | undefined
+> {
     try {
         const ticketResponse = await zafClient.get("ticket.id")
         const ticketId = ticketResponse["ticket.id"]
@@ -161,17 +45,70 @@ async function getCommentData() {
     }
 }
 
-function getAttachmentData(
+async function checkForEmbeddedImage(comment: commentObject) {
+    const subdomain = await getSubDomain()
+    const embeddedImageRegex1 = new RegExp(
+        `!\\[\\]\\((https:\\/\\/${subdomain}\\.telco\\.com\\/attachments\\/token\\/[^\\s]+\\?name=[^\\)]+)\\)`,
+        "g",
+    )
+    const embeddedImageRegex2 = new RegExp(
+        `!\\[\\]\\((https:\\/\\/${subdomain}\\.zendesk\\.com\\/attachments\\/token\\/[^\\/?]+\\/\\?name=[^\\)]+)\\)`,
+        "g",
+    )
+
+    const matches: string[] = []
+    let match
+
+    while ((match = embeddedImageRegex1.exec(comment.body)) !== null) {
+        matches.push(match[1])
+    }
+
+    while ((match = embeddedImageRegex2.exec(comment.body)) !== null) {
+        matches.push(match[1])
+    }
+    return matches
+}
+
+async function getAttachmentData(
     commentData: commentsObject,
     fileType: Array<string> | null,
     ticketID: number,
-) {
-    const collectedText = Array<collectedAttachmens>()
-    const collectedImages = Array<collectedAttachmens>()
-    const collectedPDF = Array<collectedAttachmens>()
-    const collectedOther = Array<collectedAttachmens>()
+): Promise<
+    [
+        collectedAttachmens[],
+        CollectedEmbeddedImages[],
+        collectedAttachmens[],
+        collectedAttachmens[],
+        collectedAttachmens[],
+    ]
+> {
+    const collectedText: collectedAttachmens[] = []
+    const collectedImages: collectedAttachmens[] = []
+    const collectedPDF: collectedAttachmens[] = []
+    const collectedOther: collectedAttachmens[] = []
+    const collectedEmbeddedImage: CollectedEmbeddedImages[] = []
 
     for (const comment of commentData.comments) {
+        const embeddedImageData = await checkForEmbeddedImage(comment)
+        for (const imageUrl of embeddedImageData) {
+            const parsedUrl = new URL(imageUrl)
+            const searchParams = new URLSearchParams(parsedUrl.search)
+            const filename = searchParams.get("name")
+
+            const alreadyAdded = collectedEmbeddedImage.some(
+                (obj) => obj.contentUrl === imageUrl,
+            )
+
+            if (!alreadyAdded) {
+                const obj: CollectedEmbeddedImages = {
+                    contentUrl: imageUrl,
+                    fileName: filename,
+                    timestamp: comment.created_at,
+                }
+                collectedEmbeddedImage.push(obj)
+            }
+        }
+
         if (comment.attachments.length > 0) {
             for (const attachment of comment.attachments) {
                 if (attachment.file_name === "redacted.txt") {
@@ -182,7 +119,7 @@ function getAttachmentData(
                     continue
                 }
 
-                const obj = {
+                const obj: collectedAttachmens = {
                     contentUrl: attachment.content_url,
                     fileName: attachment.file_name,
                     size: attachment.size,
@@ -209,12 +146,21 @@ function getAttachmentData(
         }
     }
 
-    return [collectedImages, collectedPDF, collectedText, collectedOther]
+    return [
+        collectedImages,
+        collectedEmbeddedImage,
+        collectedPDF,
+        collectedText,
+        collectedOther,
+    ]
 }
 
 function NavTabs(): React.ReactNode {
     const [selectedTab, setSelectedTab] = useState("Text Files")
     const [imageFiles, setImageFiles] = useState<collectedAttachmens[]>([])
+    const [embeddedImageFiles, setEmbeddedImageFiles] = useState<
+        CollectedEmbeddedImages[]
+    >([])
     const [pdfFiles, setPdfFiles] = useState<collectedAttachmens[]>([])
     const [textFiles, setTextFiles] = useState<collectedAttachmens[]>([])
     const [otherFiles, setOtherFiles] = useState<collectedAttachmens[]>([])
@@ -225,8 +171,21 @@ function NavTabs(): React.ReactNode {
             try {
                 const data = await getCommentData()
                 if (data) {
-                    const [imageData, pdfData, textData, otherData] = data
+                    const [
+                        imageData,
+                        embeddedImageData,
+                        pdfData,
+                        textData,
+                        otherData,
+                    ] = data as [
+                        collectedAttachmens[],
+                        CollectedEmbeddedImages[],
+                        collectedAttachmens[],
+                        collectedAttachmens[],
+                        collectedAttachmens[],
+                    ]
                     setImageFiles(imageData)
+                    setEmbeddedImageFiles(embeddedImageData)
                     setPdfFiles(pdfData)
                     setTextFiles(textData)
                     setOtherFiles(otherData)
@@ -242,11 +201,12 @@ function NavTabs(): React.ReactNode {
     return (
         <Tabs selectedItem={selectedTab} onChange={setSelectedTab}>
             <TabList>
-                <Tab item="Text Files">Text Files</Tab>
-                <Tab item="Images">Images</Tab>
-                <Tab item="EmbeddedImages">Embedded Images</Tab>
-                <Tab item="PDFs">PDFs</Tab>
-                <Tab item="Other">Other</Tab>
+                <Tab item="Text Files">Text Files ({textFiles.length})</Tab>
+                <Tab item="Images">
+                    Images ({imageFiles.length + embeddedImageFiles.length})
+                </Tab>
+                <Tab item="PDFs">PDFs ({pdfFiles.length})</Tab>
+                <Tab item="Other">Other ({otherFiles.length})</Tab>
             </TabList>
             <TabPanel item="Text Files">
                 {loading ? (
@@ -258,7 +218,7 @@ function NavTabs(): React.ReactNode {
                         {textFiles && textFiles.length > 0 ? (
                             <FilesTable attachments={textFiles} />
                         ) : (
-                            <p>No Text Files attachments were found.</p>
+                            <p>No text files found.</p>
                         )}
                     </>
                 )}
@@ -270,25 +230,15 @@ function NavTabs(): React.ReactNode {
                     </div>
                 ) : (
                     <>
-                        {imageFiles && imageFiles.length > 0 ? (
-                            <ImagesTable attachments={imageFiles} />
+                        {(imageFiles && imageFiles.length > 0) ||
+                        (embeddedImageFiles &&
+                            embeddedImageFiles.length > 0) ? (
+                            <ImagesTable
+                                attachedImages={imageFiles}
+                                EmbeddedImages={embeddedImageFiles}
+                            />
                         ) : (
-                            <p>No Images attachments were found.</p>
-                        )}
-                    </>
-                )}
-            </TabPanel>
-            <TabPanel item="EmbeddedImages">
-                {loading ? (
-                    <div style={{ textAlign: "center", marginTop: "20px" }}>
-                        <LoaderSkeleton items={imageFiles.length} />
-                    </div>
-                ) : (
-                    <>
-                        {imageFiles && imageFiles.length > 0 ? (
-                            <ImagesTable attachments={imageFiles} />
-                        ) : (
-                            <p>No Embedded Images attachments were found.</p>
+                            <p>No images found.</p>
                         )}
                     </>
                 )}
@@ -303,7 +253,7 @@ function NavTabs(): React.ReactNode {
                         {pdfFiles && pdfFiles.length > 0 ? (
                             <FilesTable attachments={pdfFiles} />
                         ) : (
-                            <p>No PDFs attachments were found.</p>
+                            <p>No PDF files found.</p>
                         )}
                     </>
                 )}
@@ -318,7 +268,7 @@ function NavTabs(): React.ReactNode {
                         {otherFiles && otherFiles.length > 0 ? (
                             <FilesTable attachments={otherFiles} />
                         ) : (
-                            <p>No Other attachments were found.</p>
+                            <p>No Other attachments found.</p>
                         )}
                     </>
                 )}
