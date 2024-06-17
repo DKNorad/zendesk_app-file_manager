c6a1924b24a3453fbbdbe39b30eef1c5d0823c8c
DKNorad <nedd.petrov@gmail.com>
2024-06-18 00:53:56 +0300

    Code clean up.

--- src/typescripts/App.tsx
+++ src/typescripts/App.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import { ThemeProvider, DEFAULT_THEME } from "@zendeskgarden/react-theming"
-import { Grid, Row, Col } from "@zendeskgarden/react-grid"
 import NavTabs from "./components/NavTabs"
 import { AppProps, AppState } from "./utils/interfaces"
 
@@ -14,25 +13,10 @@ class App extends React.Component<AppProps, AppState> {
         super(props)
         this._client = props.client
         this._appData = props.appData
-        this.state = { attachments: [] }
     }
 
     async componentDidMount() {
         this._client.invoke("resize", { width: "100%", height: "668px" })
-    }
-
-    async fetchAttachments() {
-        try {
-            const data = await this._client.get("ticket.conversation")
-            const attachments = data["ticket.conversation"].flatMap(
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                (attachmentObj: { attachments: any }) =>
-                    attachmentObj.attachments || [],
-            )
-            this.setState({ attachments })
-        } catch (error) {
-            console.error("Error fetching attachments:", error)
-        }
     }
 
     render() {
